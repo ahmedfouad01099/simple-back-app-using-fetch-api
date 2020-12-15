f78461b340d5e2517a7e6aec43b7d20bab6a19bd
Ahmed Fouad <ahmedfouad01099@gmail.com>
2020-12-15 11:02:33 +0200

    firsr commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const multer = require("multer");
+
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    let date = new Date().toDateString();
+    console.log(date);
+    cb(null, new Date().toISOString().replace(/[\/\\:]/g, "_") + "-" + file.originalname);
+  },
+});
+
+const fileFileter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: fileStorage, fileFilter: fileFileter }).single('image'));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+mongoose
+  .connect(
+    "mongodb+srv://ahmedfouadDB:2255@cluster0.c7jvs.mongodb.net/posts?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
